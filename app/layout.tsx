cef0a2e74c7633458d9ff1b10dfad39dfc9ce092
nate-evanich <nate.evanich@bubble.io>
2026-06-05 14:36:08 -0400

    Add the Reverse Bubble overlay snippet

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,9 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://reverse-bubble.up.railway.app/overlay/v1/rb-overlay.js" defer data-rb-project="c5d1fff9-cc27-40d6-a9a4-800c860f78a8" data-rb-token="rbo_TNZYXzmK0RmDb1fvl1_pekPc864crRwZ" />
+      </head>
       <body>{children}</body>
     </html>
   );
